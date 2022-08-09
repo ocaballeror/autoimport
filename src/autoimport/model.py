325ab46065ef50676350db59f9000bfa1ee9985b
Oscar Caballero <ocaballeror@tutanota.com>
2022-08-09 15:29:20 +0200

    Add project root to sys.path
    It is necessary to import code from the current project if it's not
    installed

--- src/autoimport/model.py
+++ src/autoimport/model.py
@@ -1,8 +1,8 @@
 """Define the entities."""
 
+import sys
 import importlib.util
 import inspect
-import os
 import re
 import pkgutil
 from importlib import import_module
@@ -374,6 +374,10 @@ class SourceCode:  # noqa: R090
             import_string: String required to import the package.
         """
         package_objects = {}
+
+        if str(here()) not in sys.path:
+            sys.path.append(str(here()))
+
         for package in self._find_project_packages():
             package_objects.update(extract_package_objects(package))
 
